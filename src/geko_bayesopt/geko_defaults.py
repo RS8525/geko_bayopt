56e20d9b2446e2677540d5211453414ebf1c56c6
Goncalo Almeida <goncalommalmeida@gmail.com>
2026-07-01 12:56:01 +0200

    corrected cjet default

--- src/geko_bayesopt/geko_defaults.py
+++ src/geko_bayesopt/geko_defaults.py
@@ -16,7 +16,7 @@ GEKO_DEFAULTS: dict[str, float] = {
     "geko_csep": 1.75,
     "geko_cnw": 0.5,
     "geko_cmix": 0.0,
-    "geko_cjet": 0.9,
+    "geko_cjet": 1.0,
     "geko_ccorner": 1.0,
     "geko_cturb": 2.0,
 }
